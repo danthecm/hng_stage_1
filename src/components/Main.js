8bf281adc1e14e405357b218360b5d285fd4f6f5
cm <etchiedaniel1@gmail.com>
2022-10-26 15:47:22 +0100

    added profile picture and name

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,11 @@
+import profile_img from "../images/profile_img.png";
+import classes from "./Main.module.css"
+
 const Main = () => {
     return (
         <main>
-            
+            <img id="profile__img" src={profile_img} alt="profile" />
+            <h2>Annette Black</h2>
         </main>
     )
 
