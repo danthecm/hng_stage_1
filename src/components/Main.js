7cd31db8b21401ed65121f165e80450028f178bc
cm <etchiedaniel1@gmail.com>
2022-10-26 16:06:52 +0100

    added the link cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,43 @@ const Main = () => {
         <main>
             <img id="profile__img" src={profile_img} alt="profile" />
             <h2>Annette Black</h2>
+
+            <div className={classes.card}>
+                <a href="#">
+                    <p>Twitter Link</p>
+                </a>
+            </div>
+
+            <div className={classes.card}>
+                <a href="#">
+                    <p>Zuri Team</p>
+                </a>
+            </div>
+
+            <div className={classes.card}>
+                <a href="#">
+                    <p>Zuri Books</p>
+                </a>
+            </div>
+
+            <div className={classes.card}>
+                <a href="#">
+                    <p>Python Books</p>
+                </a>
+            </div>
+
+            <div className={classes.card}>
+                <a>
+                    <p>Background Check for Coders</p>
+                </a>
+            </div>
+
+            <div className={classes.card}>
+                <a>
+                    <p>Design Books</p>
+                </a>
+            </div>
+
         </main>
     )
 
