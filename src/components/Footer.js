94b3b5fef89895861dd6ba138298166c61c78111
CM <etchiedaniel1@gmail.com>
2022-10-27 16:20:30 +0100

    made media queries for mobile responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,11 @@ import I4G_logo from "../images/I4G.png";
 const Footer = () => {
     return (
     <footer className={classes.footer}>
+        <div>
         <img src={zuri_logo} alt="zuri" />
         <p>HNG Internship 9 Frontend Task</p>
         <img src={I4G_logo} alt="I4G" />
+        </div>
     </footer>
     )
 }
