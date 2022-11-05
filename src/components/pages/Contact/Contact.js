21e71ed0570a9d86f298cda9c6bfd17490a06e7d
CM <etchiedanie1@gmail.com>
2022-11-05 12:10:11 +0100

    feat: added the form and styled all the inputs

---
+++ src/components/pages/Contact/Contact.js
@@ -0,0 +1,54 @@
+import classes from "./Contact.Module.css";
+
+const Contact = () => {
+  return (
+    <section className={classes.section}>
+      <h1>Contact Me</h1>
+      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      <form>
+        <div className="firstLast">
+          <label htmlFor="First Name">First Name</label>
+          <label htmlFor="Last Name">Last Name</label>
+          <input
+            type="text"
+            name="First Name"
+            id="first_name"
+            placeholder="Enter your first name"
+          />
+          <input
+            type="text"
+            name="Last Name"
+            id="last_name"
+            placeholder="Enter your last name"
+          />
+        </div>
+
+        <div className="card">
+          <label htmlFor="Email">Email</label>
+          <input
+            type="email"
+            name="Email"
+            id="email"
+            placeholder="yourname@email.com"
+          />
+        </div>
+
+        <div className="card">
+          <label htmlFor="Message">Message</label>
+          <textarea
+            name="Message"
+            id="message"
+            placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+        </div>
+
+        <div className="checkboxContainer">
+            <input type="checkbox" name="checkbox" id="checkbox" />
+            <p>You agree to providing your data to CM who may contact you.</p>
+        </div>
+
+        <button type="submit">Send message</button>
+      </form>
+    </section>
+  );
+};
+export default Contact;
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Home from "./components/pages/Home/Home";
+import Contact from "./components/pages/Contact/Contact";
 
 const Router = () => {
   return (
@@ -9,7 +10,7 @@ const Router = () => {
       <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
-            {/* <Route path="/contact" element={<Contact />} /> */}
+            <Route path="/contact" element={<Contact />} />
           </Route>
       </Routes>
     </BrowserRouter>
